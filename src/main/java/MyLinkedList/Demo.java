f02a2a4c42606d22819393b39806dd28bf7459c2
Roman horodyskyi <gorod10516@gmail.com>
2024-01-27 10:42:57 +0200

    Finish MyLinkedList

---
+++ src/main/java/MyLinkedList/Demo.java
@@ -0,0 +1,17 @@
+package MyLinkedList;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList<String>();
+        myLinkedList.add("Roma");
+        myLinkedList.add("Stepan");
+        myLinkedList.add("Mykola");
+        myLinkedList.add("Ivan");
+        System.out.println("myLinkedList.get(2) = " + myLinkedList.get(2));
+        myLinkedList.remove(2);
+        System.out.println("myLinkedList.get(2) = " + myLinkedList.get(2));
+
+    }
+
+}
--- src/main/java/MyLinkedList/MyLinkedList.java
+++ src/main/java/MyLinkedList/MyLinkedList.java
@@ -1,19 +1,70 @@
 package MyLinkedList;
 
-import java.util.LinkedList;
+public class MyLinkedList <E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
 
-public class MylinkedList {
-    LinkedList linkedList = new LinkedList();
+    public MyLinkedList() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    public void add(E element) {
+        final MyLinkedList.Node<E> l = last;
+        final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(l, element, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+    public Node<E> get(int index){
+        MyLinkedList.Node<E> x = first;
+        for (int i = 0; i < index; i++)
+            x = x.next;
+        return x;
+    }
+    public  void remove(int index){
+        MyLinkedList.Node<E> x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+        x.item = null;
+    }
+    public int size() {
+        return size;
+    }
+    public void clear() {
 
-}
-private static class Node<E> {
-    E item;
-    LinkedList.Node<E> next;
-    LinkedList.Node<E> prev;
+        for (MyLinkedList.Node<E> x = first; x != null; ) {
+            MyLinkedList.Node<E> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+    }
+    private static class Node<E> {
+        E item;
+        MyLinkedList.Node<E> next;
+        MyLinkedList.Node<E> prev;
 
-    Node(LinkedList.Node<E> prev, E element, LinkedList.Node<E> next) {
-        this.item = element;
-        this.next = next;
-        this.prev = prev;
+        Node(MyLinkedList.Node<E> prev, E element, MyLinkedList.Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        @Override
+        public String toString() {
+            return (String) item;
+        }
     }
 }
+
